06301f1940374f7b5cb2495812e3d335e5a7636c
Frey <freyandhy@gmail.com>
2021-12-01 17:46:40 +0800

    filter query only get published status

--- pages/index.js
+++ pages/index.js
@@ -50,7 +50,15 @@ export default function Home({ posts }) {
 export async function getStaticProps() {
   const notion = new Client({ auth: process.env.NOTION_API_KEY });
   const database_id = process.env.NOTION_DATABASE_ID;
-  const response = await notion.databases.query({ database_id });
+  const response = await notion.databases.query({
+    database_id,
+    filter: {
+      property: "status",
+      select: {
+        equals: "published",
+      },
+    },
+  });
 
   return {
     props: {

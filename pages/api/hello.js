a539eaae7dcaf44c3c2eecc704b485bef9ba929f
Frey <freyandhy@gmail.com>
2021-12-01 17:37:23 +0800

    first init

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const { Client } = require("@notionhq/client");
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+const database_id = process.env.NOTION_DATABASE_ID;
+
+const queryDB = async (req, res) => {
+  const response = await notion.databases.query({ database_id });
+  res.status(200).json({ response });
+};
+
+export default queryDB;

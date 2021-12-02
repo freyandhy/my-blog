8a71fbabd2372315fc9a087c4e0ce862b5962f53
Frey <freyandhy@gmail.com>
2021-12-02 20:54:14 +0800

    styling home page

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -5,7 +5,15 @@ const notion = new Client({ auth: process.env.NOTION_API_KEY });
 const database_id = process.env.NOTION_DATABASE_ID;
 
 const queryDB = async (req, res) => {
-  const response = await notion.databases.query({ database_id });
+  const response = await notion.databases.query({
+    database_id,
+    filter: {
+      property: "status",
+      select: {
+        equals: "published",
+      },
+    },
+  });
   res.status(200).json({ response });
 };
 
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      screens: {
+        sm: "100%",
+        md: "100%",
+        lg: "100%",
+        xl: "768px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto"],
+        lato: ["Lato"],
+        proza: ["Proza Libre"],
+      },
+    },
+  },
+  variants: {
+    extend: {
+      borderWidth: ["last"],
+    },
+  },
+  plugins: [],
+};

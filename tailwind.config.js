83b934cbba0e7676523c5c9b34f10548710828cf
Frey <freyandhy@gmail.com>
2021-12-07 00:20:31 +0800

    trying to use safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    "bg-green-300",
+    "bg-blue-300",
+    "bg-yellow-300",
+    "bg-red-300",
+    "hover:bg-green-600",
+    "hover:bg-blue-600",
+    "hover:bg-yellow-600",
+    "hover:bg-red-600",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     container: {
@@ -20,8 +30,6 @@ module.exports = {
   },
   variants: {
     extend: {
-      backgroundColor: ["active"],
-      textColor: ["active"],
       borderWidth: ["last"],
       fontWeight: ["hover"],
     },

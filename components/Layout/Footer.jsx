b0b068483de26cc8c0780fbb6c6435d8b6c3b8df
Friandy D. Noviandha <freyandhy@gmail.com>
2022-01-20 14:12:11 +0800

    edit footer

--- components/Layout/Footer.jsx
+++ components/Layout/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="border-t text-center font-lato py-4">
-      &copy; Friandy Dwi Noviandha 2021. Develop with NextJS and Notion. 
+      &copy; Friandy Dwi Noviandha 2021. Develop with NextJS & Notion. 
     </footer>
   );
 };
